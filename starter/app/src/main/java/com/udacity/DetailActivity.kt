076dd00f5bb3984281a50d206f586eb040206d25
DurtDiv3r <durtdev@gmail.com>
2021-02-15 11:54:02 +0200

    Added onClick to notification button to navigate back to Main Activity

--- starter/app/src/main/java/com/udacity/DetailActivity.kt
+++ starter/app/src/main/java/com/udacity/DetailActivity.kt
@@ -1,8 +1,10 @@
 package com.udacity
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_detail.*
+import kotlinx.android.synthetic.main.content_detail.*
 
 class DetailActivity : AppCompatActivity() {
 
@@ -10,6 +12,11 @@ class DetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
         setSupportActionBar(toolbar)
+
+        notification_button.setOnClickListener {
+            val intent =Intent(applicationContext, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 
 }
